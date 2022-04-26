6bd7568514de158e5400581db07fa5b090415fdf
MichaelaMatiskova <matiskova@spse-po.sk>
2022-04-26 21:32:25 +0200

    Prva verzia hada

---
+++ Snake/Assets/Snake.cs
@@ -0,0 +1,102 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private List<Transform> _segments;
+
+    public Transform segmentPrefab;
+
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        //_segments.Add(this.transform);
+
+        ResetState();
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (input != Vector2.zero)
+        //{
+         //   direction = input;
+        //}
+
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        transform.position = new Vector2(
+            Mathf.Round(transform.position.x) + _direction.x,
+            Mathf.Round(transform.position.y) + _direction.y//,
+            //0.0f
+        );
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        //_direction = Vector2.right;
+        transform.position = Vector3.zero;
+
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(transform);
+
+        //transform.position = Vector3.zero;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.tag == "Food")
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
+        }
+        //else if (other.tag == "Obstacle")
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
+    }
+}
+
+
